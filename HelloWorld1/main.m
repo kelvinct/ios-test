96d937377a6da74d371ea30fc7aa526c05637678
kelvinct <kelvinct@yahoo.com.hk>
2014-02-05 15:04:39 +0800

    Initial Commit

---
+++ HelloWorld1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld1
+//
+//  Created by kelvinct on 5/2/14.
+//  Copyright (c) 2014 kelvinct. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ HelloWorld1Tests/HelloWorld1Tests.m
@@ -0,0 +1,34 @@
+//
+//  HelloWorld1Tests.m
+//  HelloWorld1Tests
+//
+//  Created by kelvinct on 5/2/14.
+//  Copyright (c) 2014 kelvinct. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HelloWorld1Tests : XCTestCase
+
+@end
+
+@implementation HelloWorld1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
